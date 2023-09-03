36f88bfd79849a74711bf781f3f31be7fce5525d
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-03 13:35:54 -0300

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Christian Manzaraz on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeSaver/Views/Main/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  RecipeSaver
+//
+//  Created by Christian Manzaraz on 03/09/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Mis recetas")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
